d2f5f5b49b03fccafe9d4b2334e1318f5a43f420
dvorakyl <kyle.dvorak@mykenzie.snhu.edu>
2022-05-22 02:07:15 -0500

    TASK 4 COMPLETE

--- Application/src/main/java/com/kenzie/unit/four/ticketsystem/service/CloseReservationTask.java
+++ Application/src/main/java/com/kenzie/unit/four/ticketsystem/service/CloseReservationTask.java
@@ -1,7 +1,11 @@
 package com.kenzie.unit.four.ticketsystem.service;
 
+import com.kenzie.unit.four.ticketsystem.repositories.model.ReserveTicketRecord;
 import com.kenzie.unit.four.ticketsystem.service.model.ReservedTicket;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.Deque;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
 public class CloseReservationTask implements Runnable {
@@ -21,5 +25,22 @@ public class CloseReservationTask implements Runnable {
     @Override
     public void run() {
        // Your code here
+        ReservedTicket reservedTicket = reservedTicketsQueue.poll();
+        if(reservedTicket == null) {
+            return;
+        }
+        reservedTicket = reservedTicketService.findByReserveTicketId(reservedTicket.getTicketId());
+
+        Duration duration = Duration.between(LocalDateTime.parse(reservedTicket.getDateOfReservation()),
+                LocalDateTime.now());
+
+        if(duration.getSeconds() > durationToPay && !reservedTicket.getTicketPurchased()) {
+            reservedTicket = new ReservedTicket(reservedTicket.getConcertId(), reservedTicket.getTicketId(),
+                    reservedTicket.getDateOfReservation(), true, String.valueOf(LocalDateTime.now()),
+                    false);
+            reservedTicketService.updateReserveTicket(reservedTicket);
+        } else if(reservedTicket.getTicketPurchased() && duration.getSeconds() < durationToPay) {
+            reservedTicketsQueue.add(reservedTicket);
+        }
     }
 }
